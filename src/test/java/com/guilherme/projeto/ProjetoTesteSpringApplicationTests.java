0e8c61167dddeefbe9acdcf093ef6ead8b12d97a
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-02-22 23:14:33 -0300

    first commit

---
+++ src/test/java/com/guilherme/projeto/ProjetoTesteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTesteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
