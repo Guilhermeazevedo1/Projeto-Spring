6e7f167570354a8dc15b221a80e2de623f3927d1
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-02-23 21:26:46 -0300

    criação das Entitys e do repository

---
+++ src/main/java/com/guilherme/projeto/model/News.java
@@ -0,0 +1,8 @@
+package com.guilherme.projeto.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tab_news")
+public class News extends BaseComponent {
+
+}
---
+++ src/main/java/com/guilherme/projeto/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.guilherme.projeto.repository;
+
+import com.guilherme.projeto.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
